81786f9217f66be3703d35bd0cb309f83bb26b49
MidnightJames <63023750+MidnightJames@users.noreply.github.com>
2023-12-30 16:55:50 -0500

    Create Calories_Part_1.java

---
+++ day_1/Calories_Part_1.java
@@ -0,0 +1,71 @@
+package _2022_day_1;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Calories_Part_1 {
+	static final int INVALID_FILENAME = -1;
+	static final int FILE_NOT_FOUND = -2;
+	
+	public static String get_filename(Scanner keyboardScanner)
+	{
+		System.out.print("Enter file name: ");
+		String filename = keyboardScanner.nextLine();
+		
+		if (filename.isEmpty() || filename.isBlank() || filename == null)
+		{
+			System.out.println("Invalid filename entered.");
+			System.exit(INVALID_FILENAME);;
+		}
+		
+		return filename;
+	}
+	
+	public static void main(String[] args)
+	{
+		Scanner keyboardScanner = new Scanner(System.in);
+		String filename = get_filename(keyboardScanner);
+		String line = null;
+		File file = new File(filename);
+		ArrayList<Integer> elfTotalCalories = new ArrayList<Integer>();
+		int max = 0;
+		
+		try 
+		{
+			Scanner fileScanner = new Scanner(file);
+			int currentElfCalories = 0;
+			while (fileScanner.hasNextLine())
+			{
+				line = fileScanner.nextLine();
+				if (line.isBlank())
+				{
+					elfTotalCalories.add(currentElfCalories);
+					currentElfCalories = 0;
+				}
+				else
+				{
+					currentElfCalories += Integer.parseInt(line);
+				}
+			}
+			elfTotalCalories.add(currentElfCalories);
+			fileScanner.close();
+		}
+		catch (FileNotFoundException exception)
+		{
+			System.out.println("File not found.");
+			System.exit(FILE_NOT_FOUND);
+		}
+		
+		for (Integer calories : elfTotalCalories)
+		{
+			if (calories > max)
+			{
+				max = calories;
+			}
+		}
+		
+		System.out.println("Max = " + max);
+	}
+}
